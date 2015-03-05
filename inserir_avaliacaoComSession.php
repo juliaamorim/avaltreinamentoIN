151404be2a91c9c02141d9e77d6fd6dd43bdfedd
Julia Amorim <mj_amorim@id.uff.br>
2015-03-05 03:19:51 -0300

    Inserir Avaliação - ( com var session)
    Correção usando o ID da empresa do usuário atual

---
+++ inserir_avaliacaoComSession.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<?php
+   require_once('scripts/session.php');
+   require_once('scripts/bd.php');
+   //Acesso permitido somente a usuários de nível adminDeus
+   session_validaLoginRedirect( 'admin' ,'adminGeral','adminDeus');
+?>
+<html>
+<head>
+    <title>IN Junior</title>
+    <meta charset="utf-8"/>
+</head>
+<body>
+    
+    <section id="main">    
+    <nav>
+    <section id="main">
+    <article id="content">   
+ 
+        <form method='POST' action="?acao=ok"  enctype="multipart/form-data"> 
+            <fieldset>
+                <center>
+                    <legend>Inserir Avaliação</legend>
+                </center>
+                    <br>
+                    ATENÇÃO! 
+                    <br>
+                    *Todos os campos são obrigatórios
+                    <br><br>
+                                        
+                    <label for="nome">Nome</label>
+                    <input type="text" placeholder="Digite o nome"  name="nome" />
+                     
+                    <br><br>
+                     
+                    <label for="nome">Data de Criação:</label>
+                    <?php
+                     echo $data = date("d/m/Y"); 
+                     ?>
+                     
+                    <br><br>
+ 
+                    <label for="nome">Descrição: </label>
+                    <textarea  name="descricao"  placeholder="Digite aqui" rows=5 cols=35/></textarea>
+                     
+                    <label for="nome">Status: </label>
+                    <select name="aberta">
+                        <option selected>Aberta</option>
+                        <option>Concluida</option>
+                    </select>  
+                     
+                    <br><br>
+
+                    <label for="nome"> ID Empresa </label>
+                    <?php
+                    //Obtendo ID da empresa.
+                       $id_empresa = $_GET['id_empresa'];
+                       $mysqli = bd_inicia();
+                       $nome = 'SELECT nome FROM empresas WHERE id = $id_empresa';
+                   ?>
+                    
+                    <button type="submit" name="enviar">Inserir</button>
+ 
+            </fieldset>
+        </form>
+    </article>
+         
+         
+    </section>
+     
+     
+</body>
+</html>
+ 
+<?php   
+
+    if(isset($_POST['enviar'])){ #Informa se a variável foi iniciada
+
+        $nome = $_POST['nome'];
+        $descricao = $_POST['descricao'];
+        $aberta = $_POST['aberta'];
+         
+        if($nome == ''){
+            echo 'ATENÇÃO!! Campo NOME encontra-se vazio.';
+        } 
+
+        elseif ($descricao == '') {
+            echo 'ATENÇÃO!! Campo DESCRIÇÃO encontra-se vazio.';
+        }
+         
+        else{
+                      
+            $mysqli = new mysqli('localhost','root','injunior','avaltreinamento');
+             
+            if (mysqli_connect_errno()){
+                die('Não foi possível conectar-se ao banco de dados.<a href="inserir_avaliacao.php"> Tente novamente</a>'/*.mysqli_connect_error()*/);
+            }
+                     
+            if($mysqli){
+                 
+                mysqli_set_charset($mysqli, 'utf8');
+                 
+                $sql = "INSERT INTO aval (nome, dt_criacao, descricao, aberta, id_empresa)"
+                ."values ('$nome', ?, '$descricao', '$aberta', ?)";
+                 
+                  
+                $stmt = $mysqli->prepare($sql);
+                 
+                 
+                $ok = $stmt->execute();
+                 
+                if($ok){ ?>
+                    <SCRIPT LANGUAGE="JavaScript" TYPE="text/javascript">
+                        alert ("Avaliação inserida com sucesso");
+                    </SCRIPT>
+ 
+                <?php
+                }
+                else{
+                    echo "<SCRIPT LANGUAGE=\"JavaScript\" TYPE=\"text/javascript\">
+                        alert (\"Não foi possivel inserir a avaliação, por favor, tente novamente\");
+                    </SCRIPT>";
+                     
+                }  
+ 
+                mysqli_close($mysqli);
+            }
+        }
+}      
+?>      
