0c929ed4d2811933f971d505b62f7506d4dbcd0f
Anderson Teixeira <anderson_smash@hotmail.com>
2015-03-21 18:13:08 -0300

    Alterar senha modificado

--- alterarSenha.php
+++ alterarSenha.php
@@ -1,15 +1,23 @@
-<?php include("functions.php") ?>
+<?php 
+	require_once('scripts/functions.php');
+?>
 
 <?php
-	function alterar_Senha($conexao, $strNome, $strSenhaAtual, $strConfNovaSenha){
+	function alterar_Senha($conexao, $strEmail, $strSenhaAtual, $strConfNovaSenha){
+
+		//CRIPTOGRAFA A SENHA PARA COMPARA-LA COM A DO BANCO DE DADOS
+		$strSenhaAtual = sha1($strSenhaAtual);
+
+		//CRIPTOGRAFA A SENHA PARA INSERI-LA NO BANCO DE DADOS
+		$strConfNovaSenha = sha1($strConfNovaSenha);
 
 		mysqli_set_charset($conexao,'utf-8');
-		$query = "UPDATE usuarios SET senha = '$strConfNovaSenha' WHERE  nome = '$strNome' AND senha = '$strSenhaAtual'";
-		$conexao->query($query);
-		
-		echo "Atualização bem sucedida!!";
 
-		header('Location:home.php');
+		$query = "UPDATE usuarios SET senha = '$strConfNovaSenha' WHERE  email = '$strEmail' AND senha = '$strSenhaAtual'";
+		$conexao->query($query);
+		//"Atualização bem sucedida!!";
+
+		header('Location:index.php');
 		
 		$conexao->close();
 	}
@@ -18,8 +26,14 @@
 <!doctype html>
 <html>
 	<head>
-		<meta charset="UTF-8"/>
+		<meta charset="UTF-8">
 		<title>Alterar Senha</title>
+		<link 
+		href="css/style.css" 
+		title="style" 
+		type="text/css" 
+		rel="stylesheet"
+		media="all"/>
 	</head>
 	<body>
 		<form action="" method="POST" enctype="multipart/form-data">
@@ -34,11 +48,12 @@
 			<input type="password" name="confNovaSenha"/><br><br>
 			
 			<input type="submit" name="salvar" value="Salvar Alterações"><br>
+			
 			<?php
-				session_start();
 				
 				if (isset($_POST['salvar'])) {
-					$strNome = $_SESSION['nome'];
+					
+					$strEmail = $_SESSION['email'];
 					$strSenhaAtual = $_POST['senhaAtual'];
 					$strNovaSenha = $_POST['novaSenha'];
 					$strConfNovaSenha = $_POST['confNovaSenha'];
@@ -48,7 +63,7 @@
 					} else {
 						if($strNovaSenha == $strConfNovaSenha){
 							$conexao = bd_conecta();
-							alterar_Senha($conexao, $strNome, $strSenhaAtual, $strConfNovaSenha);
+							alterar_Senha($conexao, $strEmail, $strSenhaAtual, $strConfNovaSenha);
 						}
 					}
 				}
